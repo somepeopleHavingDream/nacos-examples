d8511194f714dff0e5cd68a8fff7452ca16a626b
yangxin <1393003255@qq.com>
2022-09-13 17:54:19 +0800

    feat: nacos，启动服务发现

---
+++ nacos-spring-boot-example/nacos-spring-boot-discovery-example/src/main/java/org/yangxin/nacos/discovery/NacosDiscoveryApplication.java
@@ -0,0 +1,17 @@
+package org.yangxin.nacos.discovery;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangxin
+ * 2022/9/13 16:58
+ */
+@SpringBootApplication
+public class NacosDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryApplication.class, args);
+    }
+}
