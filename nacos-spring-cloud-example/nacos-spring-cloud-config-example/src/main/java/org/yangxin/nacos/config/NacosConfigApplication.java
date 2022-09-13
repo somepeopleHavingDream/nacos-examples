30b6b2851089d6c9d109528d46a9dd59a15fca99
yangxin <1393003255@qq.com>
2022-09-13 19:37:08 +0800

    feat: nacos，cloud，启动服务发现

---
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/org/yangxin/nacos/config/NacosConfigApplication.java
@@ -0,0 +1,16 @@
+package org.yangxin.nacos.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangxin
+ * 2022/9/13 18:23
+ */
+@SpringBootApplication
+public class NacosConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
---
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/org/yangxin/nacos/config/controller/ConfigController.java
@@ -0,0 +1,24 @@
+package org.yangxin.nacos.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yangxin
+ * 2022/9/13 18:25
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
